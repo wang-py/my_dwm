c5ca424b2e86d9666f8fd1b84f4a453ea73c6782
MillerMC96 <pywang@ucdavis.edu>
2020-07-18 01:21:59 -0700

    noto font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monaco:size=10" };
-static const char dmenufont[]       = "monaco:size=10";
+static const char *fonts[]          = { "noto mono:size=10" };
+static const char dmenufont[]       = "noto mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
