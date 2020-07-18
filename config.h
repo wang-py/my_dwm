de0c982f47c290e3b2a9302f637f8b08a16fe1dc
MillerMC96 <pywang@ucdavis.edu>
2020-07-18 01:29:09 -0700

    different color

--- config.h
+++ config.h
@@ -5,17 +5,18 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monaco:size=10" };
-static const char dmenufont[]       = "monaco:size=10";
+static const char *fonts[]          = { "monaco:size=11" };
+static const char dmenufont[]       = "monaco:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_blue[]        = "#4891b5";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_blue,  col_blue  },
 };
 
 /* tagging */
