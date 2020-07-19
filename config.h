08ecb0bb316ea47f29f378f96110c8dd16d171ea
MillerMC96 <pywang@ucdavis.edu>
2020-07-18 18:05:26 -0700

    slightly bigger step

--- config.h
+++ config.h
@@ -101,8 +101,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ 0, XF86XK_AudioMute,			spawn,		SHCMD("pamixer -t; kill -39 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 3; kill -39 $(pidof dwmblocks)") },
-	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 3; kill -39 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("pamixer --allow-boost -i 5; kill -39 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("pamixer --allow-boost -d 5; kill -39 $(pidof dwmblocks)") },
 };
 
 /* button definitions */
