908b45c87ae0436282abe9fcbc30a89807cb9aa7
MillerMC96 <pywang@ucdavis.edu>
2020-08-07 12:49:56 -0700

    bigger font and added emojis

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monaco:size=11" };
-static const char dmenufont[]       = "monaco:size=11";
+static const char *fonts[]          = { "monaco:size=12" };
+static const char dmenufont[]       = "monaco:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4" }; //, "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "🌐", "♫", "📺"}; //, "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
