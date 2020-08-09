9a37e740c8a0ff6c3f1a9b26d59281e1d8180b57
MillerMC96 <pywang@ucdavis.edu>
2020-08-09 12:31:11 -0700

    consistent colors in dmenu

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray5, NULL };
 static const char *termcmd[]  = { "konsole", NULL };
 /* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
 //static char *statuscmds[] = { "notify-send Mouse$BUTTON" };
