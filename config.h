08b2292f2b1583d8f501545e6ef8e2d6cf2c94c6
MillerMC96 <pywang@ucdavis.edu>
2020-08-22 22:59:30 -0700

    smaller font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monaco:size=12", "fontawesome:size=12", "noto sans sc:size=12" }; //
-static const char dmenufont[]       = "monaco:size=12";
+static const char *fonts[]          = { "monaco:size=10", "fontawesome:size=10", "noto sans sc:size=10" }; //
+static const char dmenufont[]       = "monaco:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
