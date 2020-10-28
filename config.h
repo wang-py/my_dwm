524e507d772c72327f2393b6cce4574d30a55f06
MillerMC96 <pywang@ucdavis.edu>
2020-10-27 18:55:08 -0700

    fixed comments

--- config.h
+++ config.h
@@ -33,7 +33,7 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
+	/* class      		instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
 	{ "Code",     		NULL,       NULL,       1,	  	      0,           -1 },
 	{ "konsole",   	 	NULL,       NULL,       1 << 1,       0,           -1 },
