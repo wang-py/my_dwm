73c67de3eb2d6dffde983bc6b90def38e5d24f1a
MillerMC96 <pywang@ucdavis.edu>
2020-10-27 18:46:46 -0700

    using custom rules

--- config.h
+++ config.h
@@ -34,8 +34,12 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Gimp",     		NULL,       NULL,       0,            1,           -1 },
+	{ "Code",     		NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "konsole",   	 	NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,       1 << 3,       0,           -1 },
+	{ "Spotify",    	NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "Firefox",  		NULL,       NULL,       1 << 5,       0,           -1 },
 };
 
 /* layout(s) */
